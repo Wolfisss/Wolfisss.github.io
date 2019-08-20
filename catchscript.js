eae20e5bbb7fc3a069e427ee3d045d7535d36de3
Wolfisss <54003129+Wolfisss@users.noreply.github.com>
2019-08-20 21:56:08 +0400

    Update catchscript.js

--- catchscript.js
+++ catchscript.js
@@ -2,7 +2,7 @@ let im = document.querySelector("img");
 im.addEventListener("click", gStart);
 function gStart(){
  im.style ="display: none";
- var cats = 0, miss = 0, level = 1;
+ var cats = 1, miss = 0, level = 1;
  var p1 = document.getElementById("p1");
  var p2 = document.getElementById("p2");
  var p3 = document.getElementById("p3");
@@ -11,7 +11,7 @@ function gStart(){
  p2.textContent = "miss: " + miss.toString();
  p3.textContent = "level: " + level.toString();
  var canv = document.createElement("canvas");
- canv.style = "background: white";
+ canv.style.background = white;
  md.appendChild(canv);
  canv.style.width = "668px";
  canv.style.height = "558px";
