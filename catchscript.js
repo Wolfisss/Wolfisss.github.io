c399093d4ec4e480d8ad4bcb960391d8e8fd9175
Wolfisss <54003129+Wolfisss@users.noreply.github.com>
2019-08-20 21:09:48 +0400

    Update catchscript.js

--- catchscript.js
+++ catchscript.js
@@ -4,5 +4,10 @@ function gStart(){
  im.style ="display: none";
  var cats = 0, miss = 0, level = 1;
  var p1 = document.getElementById("p1");
+ var p2 = document.getElementById("p2");
+ var p3 = document.getElementById("p3");
+ var md = document.getElementById("mdiv");
  p1.textContent = "cats: " + cats.toString();
+ p2.textContent = "miss: " + miss.toString();
+ p3.textContent = "level: " + level.toString();
 }
