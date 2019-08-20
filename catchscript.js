bae884f52d61bc30b4c77e2ef995280a8697f6fb
Wolfisss <54003129+Wolfisss@users.noreply.github.com>
2019-08-20 20:43:42 +0400

    Update catchscript.js

--- catchscript.js
+++ catchscript.js
@@ -3,10 +3,8 @@ im.addEventListener("click", gStart);
 function gStart(){
  im.style ="display: none;
             position: relative;
-            top: 600;";
- let cats = 0, 
-     miss = 0,
-     level = 1;
+            top: 600px;";
+ var cats = 0, miss = 0, level = 1;
  let divAll = document.querySelectorAll("div");
  divall[0].textContent = "cats: " + cats.toString();
 }
