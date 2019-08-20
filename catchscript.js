2e73d80d4b19fea69cded23eccf8f7ff37d586a6
Wolfisss <54003129+Wolfisss@users.noreply.github.com>
2019-08-20 21:27:10 +0400

    Update catchscript.js

--- catchscript.js
+++ catchscript.js
@@ -11,7 +11,7 @@ function gStart(){
  p2.textContent = "miss: " + miss.toString();
  p3.textContent = "level: " + level.toString();
  var canv = document.createElement("canvas");
- canv.style = "background: white";
+ canv.style.background = white;
  md.appendChild(canv);
  cx = canv.getContext("2d");
 }
