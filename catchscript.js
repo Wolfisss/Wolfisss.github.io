cef92ceef8c50c3d8b0ed69f14143eb7ae3408d8
Wolfisss <54003129+Wolfisss@users.noreply.github.com>
2019-08-21 17:42:43 +0400

    Update catchscript.js

--- catchscript.js
+++ catchscript.js
@@ -23,12 +23,22 @@ function gStart(){
   let b = Math.floor(Math.random()*256);
   md.style.background = "#" + r.toString(16) +
    g.toString(16) + b.toString(16);
-  cx.drawImage(im, getCoord(2).x,getCoord(2).y,150,150,
+  cx.drawImage(im, getCoord(3).x,getCoord(3).y,150,150,
    Math.floor(Math.random()*668),Math.floor(Math.random()*558),150,150);
   function getCoord(icat) {
    switch (icat) {
     case 1: return {x: 0, y: 0}; break;
     case 2: return {x: 170, y: 0}; break;
+    case 3: return {x: 340, y: 0}; break;
+    case 4: return {x: 0, y: 0}; break;
+    case 5: return {x: 170, y: 0}; break;
+    case 6: return {x: 340, y: 0}; break;
+    case 7: return {x: 0, y: 0}; break;
+    case 8: return {x: 170, y: 0}; break;
+    case 9: return {x: 340, y: 0}; break;
+    case 10: return {x: 0, y: 0}; break;
+    case 11: return {x: 170, y: 0}; break;
+    case 12: return {x: 340, y: 0}; break;
    }
   }
  }
