2441d446b6704a0391a5637a174ccf9386377b53
Wolfisss <54003129+Wolfisss@users.noreply.github.com>
2019-08-21 15:46:58 +0400

    Update catchscript.js

--- catchscript.js
+++ catchscript.js
@@ -23,4 +23,6 @@ function gStart(){
   let b = Math.floor(Math.random()*256);
   md.style.background = "#" + r.toString(16) +
    g.toString(16) + b.toString(16);
+  cx.drawImage(im, 0,0,150,150,300,300,150,150);
+ }
 }
