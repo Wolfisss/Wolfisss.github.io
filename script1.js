8d6757ac24ebec44006a7069805537c8b5d48875
Wolfisss <54003129+Wolfisss@users.noreply.github.com>
2019-08-27 17:38:49 +0400

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,2 +1,15 @@
-let f1 = function() {alert("hello");}
+function f1() {
+  var d1 = document.createElement("div");
+  d1.style.class = "knopka";
+  d1.style.opacity = 0;
+  document.body.appendChild(d1);
+  var rId = requestAnimationFrame(f2);
+  var lastTime = null;
+  function f2(time) {
+    if (lastTime == null) lastTime = time;
+    d1.style.opacity += 0.001*(time - lastTime)*0.01;
+    if (d1.style.opacity == 1) cancelAnimationFrame(rId);
+    else requestAnimationFrame(f2);
+  }
+}
 window.addEventListener("load", f1);
