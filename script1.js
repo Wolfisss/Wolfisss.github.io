4b02ce2f7ddc076fe99a66bca15265d947aa0f2a
Wolfisss <54003129+Wolfisss@users.noreply.github.com>
2019-08-27 17:45:02 +0400

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,7 +1,7 @@
 function f1() {
   var d1 = document.createElement("div");
   d1.style.class = "knopka";
-  d1.style.opacity = 0.5;
+  d1.style.opacity = "0.5";
   document.body.appendChild(d1);
  /* var rId = requestAnimationFrame(f2);
   var lastTime = null;
