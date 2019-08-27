ccbfe52bccd34da60619380feb8eb0ce3a1f89aa
Wolfisss <54003129+Wolfisss@users.noreply.github.com>
2019-08-27 18:19:49 +0400

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,15 +1,15 @@
 function f1() {
   var d1 = document.createElement("div");
   d1.classList.add("knopka");
-  d1.style.opacity = 0.5;
+  d1.style.opacity = 0;
   document.body.appendChild(d1);
- /* var rId = requestAnimationFrame(f2);
   var lastTime = null;
+  var rId = requestAnimationFrame(f2);
   function f2(time) {
     if (lastTime == null) lastTime = time;
     d1.style.opacity += 0.001*(time - lastTime)*0.01;
     if (d1.style.opacity == 1) cancelAnimationFrame(rId);
     else requestAnimationFrame(f2);
-  } */
+  }
 }
 window.addEventListener("load", f1);
