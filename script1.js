e53bb58d8abb334c96042de7c18faa1a92e3f4d1
Wolfisss <54003129+Wolfisss@users.noreply.github.com>
2019-08-28 12:26:16 +0400

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,19 +1,24 @@
+var scena = 1;
 function f1() {
-  var d1 = document.createElement("div");
-  d1.classList.add("knopka");
-  d1.style.opacity = 0;
-  document.body.appendChild(d1);
-  var lastTime = null; var opop = 0;
-  var rId = requestAnimationFrame(f2);
-  function f2(time) {
-    if (lastTime == null) lastTime = time;
-    opop += 0.001*(time - lastTime);
-    lastTime = time;
-    if (opop >= 1) cancelAnimationFrame(rId);
-    else {
-      d1.style.opacity = opop;
-      requestAnimationFrame(f2);
-    }
-  }
+  switch (scena) {
+    case 1: {
+      var d1 = document.createElement("div");
+      d1.classList.add("knopka");
+      d1.style.opacity = 0;
+      document.body.appendChild(d1);
+      var lastTime = null; var opop = 0;
+      var rId = requestAnimationFrame(f2);
+      function f2(time) {
+        if (lastTime == null) lastTime = time;
+        opop += 0.001*(time - lastTime);
+        lastTime = time;
+        if (opop >= 1) cancelAnimationFrame(rId);
+        else {
+          d1.style.opacity = opop;
+          requestAnimationFrame(f2);
+        }
+      }
+    } break;
+  } 
 }
 window.addEventListener("load", f1);
